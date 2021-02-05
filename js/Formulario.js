de0fc94cd50b9f5e8b063d76b3ceb93659bd2369
Christian <christian_8_neus@hotmail.com>
2021-02-05 14:55:53 +0100

    done

--- js/Formulario.js
+++ js/Formulario.js
@@ -1,28 +1,33 @@
 "use strict";
 
-class Login{
-    constructor(){
-        this.nameInput = document.querySelector("#name");
-        this.emailInput = document.querySelector("#email");
-        this.termsInput = document.querySelector("#terms");
-        this.sendButton = document.querySelector("#send");
-        this.messageContainer = document.querySelector(".message-container")
+var nombre = document.getElementById('name');
+var email = document.getElementById('email');
+var terms = document.getElementById('terms');
+var error = document.getElementById('error');
+var exito = document.getElementById('exito');
+
+
+function enviarFormulario(){
+    var mensajesError = [];
+    var mensajesExito = [];
+
+    if(nombre.value === "" || nombre.value === null){
+        mensajesError.push('Por favor rellena el campo nombre')
     }
 
-    handleEmailInput = (event) => {
-        const email = event.target.value;
+    if(email.value === "" || email.value === null){
+        mensajesError.push('Por favor escribe un email valido')
+    }
     
-        validator.validateValidEmail(email);
-    
-        const errorsObj = validator.getErrors();
-    
-        if(!errorsObj.invalidEmailError){
-            validator.validateUniqueEmail(email)
-        }
-    
-    
-        this.setErrorMessages();
-        this.checkButton();
+    if(terms.checked != true){
+        mensajesError.push('Hay que aceptar las condiciones')
     }
 
+    if(!mensajesError){
+        mensajesExito.push("Hi " +nombre)
+    }
+    error.innerHTML = mensajesError.join(', ');
+    exito.innerHTML = mensajesExito.join(', ');
+
+    return false;
 }
